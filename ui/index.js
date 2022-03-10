a1f850a6dd5f1e94104328a9f524ad99f5c8ccc1
Stepan Bryl <bryl.step@tut.by>
2022-03-10 11:07:40 +0300

    init: js

---
+++ ui/index.js
@@ -0,0 +1,3 @@
+import tweets from './js/tweet.js';
+import comments from './js/comment.js';
+
---
+++ ui/js/comment.js
@@ -0,0 +1,76 @@
+const comments = [
+    {
+        id: '100',
+           text: 'Хорошо, а у тебя?',
+           createdAt: new Date('2022-03-09T23:00:05'),
+           author: 'Иванов Иван' 
+    },
+    {
+    id: '101',
+    text: 'Хорошо, а у тебя?',
+    createdAt: new Date('2022-03-09T23:00:05'),
+    author: 'Иванов Иван'
+    },
+    {
+        id: '102',
+           text: 'Хорошо, а у тебя?',
+           createdAt: new Date('2022-03-09T23:00:05'),
+           author: 'Иванов Иван' 
+    },
+    {
+        id: '103',
+           text: 'Хорошо, а у тебя?',
+           createdAt: new Date('2022-03-09T23:00:05'),
+           author: 'Иванов Иван' 
+    },
+    {
+        id: '104',
+           text: 'Хорошо, а у тебя?',
+           createdAt: new Date('2022-03-09T23:00:05'),
+           author: 'Иванов Иван' 
+    },
+    {
+        id: '105',
+           text: 'Хорошо, а у тебя?',
+           createdAt: new Date('2022-03-09T23:00:05'),
+           author: 'Иванов Иван' 
+    },
+    {
+        id: '106',
+           text: 'Хорошо, а у тебя?',
+           createdAt: new Date('2022-03-09T23:00:05'),
+           author: 'Иванов Иван' 
+    },
+    {
+        id: '107',
+           text: 'Хорошо, а у тебя?',
+           createdAt: new Date('2022-03-09T23:00:05'),
+           author: 'Иванов Иван' 
+    },
+    {
+        id: '108',
+           text: 'Хорошо, а у тебя?',
+           createdAt: new Date('2022-03-09T23:00:05'),
+           author: 'Иванов Иван' 
+    },
+    {
+        id: '109',
+           text: 'Хорошо, а у тебя?',
+           createdAt: new Date('2022-03-09T23:00:05'),
+           author: 'Иванов Иван' 
+    },
+    {
+        id: '110',
+           text: 'Хорошо, а у тебя?',
+           createdAt: new Date('2022-03-09T23:00:05'),
+           author: 'Иванов Иван' 
+    },
+    {
+        id: '111',
+           text: 'Хорошо, а у тебя?',
+           createdAt: new Date('2022-03-09T23:00:05'),
+           author: 'Иванов Иван' 
+    }
+]
+
+export default comments;
