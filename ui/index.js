ea9edd96e542dc2c58c5e3d181549f1a97bc061c
Stepan Bryl <bryl.step@tut.by>
2022-03-12 19:55:24 +0300

    feat: index.js getTweets, getTweet, validateTweet

--- ui/index.js
+++ ui/index.js
@@ -1,3 +1,233 @@
-import tweets from './js/tweet.js';
-import comments from './js/comment.js';
+'use strict'
+const tweets = [
+    {
+       id: '1',
+       text: 'Привет! #js #datamola',
+       createdAt: new Date('2022-03-09T23:00:00'),
+       author: 'Калякин Иван',
+       comments: []
+  
+    },
+    {
+       id: '2',
+       text: 'Какие дела?',
+       createdAt: new Date('2022-03-08T23:00:01'),
+       author: 'Брыль Степан',
+       comments: [{
+           id: '912',
+           text: 'Хорошо, а у тебя?',
+           createdAt: new Date('2022-03-09T23:00:05'),
+           author: 'Иванов Иван'
+       }]
+    },
+    {
+        id: '3',
+        text: 'Привет! #js #datamola',
+        createdAt: new Date('2022-03-07T23:00:00'),
+        author: 'Иванов Степан',
+        comments: []
+   
+     },
+     {
+        id: '4',
+        text: 'Привет! #js #datamola',
+        createdAt: new Date('2022-03-09T23:00:00'),
+        author: 'Николаев Иван',
+        comments: []
+   
+     },
+     {
+        id: '5',
+        text: 'Привет! #js #datamola',
+        createdAt: new Date('2022-03-09T23:00:00'),
+        author: 'Иванов Иван',
+        comments: []
+   
+     },
+     {
+        id: '6',
+        text: 'Привет! #js #datamola',
+        createdAt: new Date('2022-03-09T23:00:00'),
+        author: 'Николаев Иван',
+        comments: [{
+            id: '111',
+           text: 'Хорошо, а у тебя?',
+           createdAt: new Date('2022-03-09T23:00:05'),
+           author: 'Иванов Иван'
+        }]
+   
+     },
+     {
+        id: '7',
+        text: 'Привет! #js #datamola',
+        createdAt: new Date('2022-03-09T23:00:00'),
+        author: 'Калякин Иван',
+        comments: []
+   
+     },
+     {
+        id: '8',
+        text: 'Привет! #js #datamola',
+        createdAt: new Date('2022-03-09T23:00:00'),
+        author: 'Николаев Иван',
+        comments: []
+   
+     },
+     {
+        id: '9',
+        text: 'Привет! #js #datamola',
+        createdAt: new Date('2022-03-09T23:00:00'),
+        author: 'Малякин Иван',
+        comments: []
+   
+     },
+     {
+        id: '10',
+        text: 'Привет! #js #datamola',
+        createdAt: new Date('2022-03-09T23:00:00'),
+        author: 'Калякин Иван',
+        comments: []
+   
+     },
+     {
+        id: '11',
+        text: 'Привет! #js #datamola',
+        createdAt: new Date('2022-03-09T23:00:00'),
+        author: 'Малякин Иван',
+        comments: []
+   
+     },
+     {
+        id: '12',
+        text: 'Привет! #js #datamola',
+        createdAt: new Date('2022-03-09T23:00:00'),
+        author: 'Колугин Иван',
+        comments: [{
+            id: '100',
+               text: 'Хорошо, а у тебя?',
+               createdAt: new Date('2022-03-09T23:00:05'),
+               author: 'Иванов Иван' 
+        },
+        {
+        id: '101',
+        text: 'Хорошо, а у тебя?',
+        createdAt: new Date('2022-03-09T23:00:05'),
+        author: 'Иванов Иван'
+        }]
+   
+     },
+     {
+        id: '13',
+        text: 'Привет! #js #datamola',
+        createdAt: new Date('2022-03-09T23:00:00'),
+        author: 'Иванов Иван',
+        comments: []
+   
+     },
+     {
+        id: '14',
+        text: 'Привет! #js #datamola',
+        createdAt: new Date('2022-03-09T23:00:00'),
+        author: 'Калякин Иван',
+        comments: []
+   
+     },
+     {
+        id: '15',
+        text: 'Привет! #js #datamola',
+        createdAt: new Date('2022-03-09T23:00:00'),
+        author: 'Малякин Иван',
+        comments: []
+   
+     },
+     {
+        id: '16',
+        text: 'Привет! #js #datamola',
+        createdAt: new Date('2022-03-09T23:00:00'),
+        author: 'show',
+        comments: []
+   
+     },
+     {
+        id: '17',
+        text: 'Привет! #js #datamola',
+        createdAt: new Date('2022-03-09T23:00:00'),
+        author: 'Калякин Иван',
+        comments: []
+   
+     },
+     {
+        id: '18',
+        text: 'Привет! #js #datamola',
+        createdAt: new Date('2022-03-07T23:00:00'),
+        author: 'Колугин Иван',
+        comments: []
+   
+     },
+     {
+        id: '19',
+        text: 'Привет! #js #datamola',
+        createdAt: new Date('2022-03-08T23:00:00'),
+        author: 'Николаев Иван',
+        comments: []
+   
+     },
+     {
+        id: '20',
+        text: 'Привет! #js #datamola',
+        createdAt: new Date('2022-03-09T23:00:00'),
+        author: 'Колугин Иван',
+        comments: []
+   
+     }
+  ];
 
+   const tweetFunc = (function() {
+      const user = 'Stepan Bryl'
+   function getTweets(skip = 0, top = 10, filterConfig) { 
+     if(filterConfig){
+     filterTweets(filterConfig);
+     }
+     // sort here;
+      if(skip >= 0 && skip <= top){
+         return tweets.slice(skip, skip+10)
+      }else{
+     return "invalid parameter";
+      }
+    }
+    function filterTweets(filterConfig) {
+      return tweets.filter(tweet => {
+        let authorFilter, textFilter, dateFromFilter, dateToFilter, hashtagsFilter;
+        authorFilter = textFilter = dateFromFilter = dateToFilter = hashtagsFilter = true;
+        if(filterConfig?.author){
+        authorFilter = tweet.author.includes(filterConfig.author);
+         }
+       if(filterConfig?.text){
+       textFilter = tweet.text.includes(filterConfig.text);
+       }
+       if(filterConfig?.dateFrom){
+          dateFromFilter = tweet.createdAt <= filterConfig.dateFrom;
+       }
+       if(filterConfig?.dateTo){
+          dateToFilter = tweet.createdAt >= filterConfig.dateTo;
+       }
+       if(filterConfig?.hashtags){
+         hashtagsFilter = filterConfig.hashtags.every(item => tweet.text.includes(item));
+       }
+       return authorFilter && textFilter && dateFromFilter && dateToFilter && hashtagsFilter;
+      })
+  }
+   const getTweet = (id) => {
+   return tweets.find(function(item) {
+         if(id)
+         return (item.id === id)
+     })
+    };   
+    function validateTweet(tweet){
+      const isValidComments = Array.isArray(tweet.comments) && (tweet.comments.length === 0 || tweet.comments.every(comment => !!comment.id && !!comment.text 
+          && !!comment.createdAt?.getMonth && !!comment.author));
+         return !!tweet.id && !!tweet.text && !!tweet.author && !!tweet.createdAt?.getMonth && isValidComments;  
+  }
+   return {getTweets, getTweet, validateTweet};
+  }());
+ 
