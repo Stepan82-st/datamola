272a53a2a1b63eea701bcd69521d5726f4283ea6
Stepan Bryl <bryl.step@tut.by>
2022-03-20 17:18:40 +0300

    feat: task4.2

--- extra task/task4.2/index.js
+++ extra task/task4.2/index.js
@@ -27,7 +27,19 @@ function div(a,b){
         return div.bind(null, a)
     }
 }
+sub =_.curry(sub);
+mul =_.curry(mul);
+div=_.curry(div);
+add=_.curry(add);
+
 let k = 30;
   let sub1 = sub(1); 
 let c = sub1(k);
   console.log(c)
+  let a = add(1,2); 
+let b = mul(a, 10);
+console.log(a);
+console.log(b);
+
+
+
